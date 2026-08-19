7205ac4d594e996f53fd33014b80b5f96ace5fdd
Irfan Khalid <i232092@isb.nu.edu.pk>
2026-08-19 20:51:12 +0500

    shared parameters and paths added in config.py

---
+++ src/config.py
@@ -0,0 +1,19 @@
+
+'''
+Shared configuration parameters for the simulation.
+'''
+
+'''Gabor Click Parameters'''
+F0 = 20_000 # center frequency (Hz)
+T0 = 0.005 # click center time (s)
+SIGMA = 0.0005 # standard deviation of the Gaussian envelope (s)
+A = 1 # amplitude of the click
+PHI = 0 # phase of the click (radians)
+DURATION = 0.01 # duration of the click (s)
+FS = 500_000 # sampling frequency (Hz)
+
+'''Paths'''
+FIGURES_DIR = "results/figures"
+METRICS_DIR = "results/metrics"
+
+
